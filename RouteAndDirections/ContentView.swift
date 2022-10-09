de7523744d221e3e32d8b27c6680c2234c3311c2
Jacek Kosiński <kashiash@gmail.com>
2022-10-09 16:17:22 +0200

    base routing

--- RouteAndDirections/ContentView.swift
+++ RouteAndDirections/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
@@ -13,7 +14,10 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Directions!")
+           
+              MapView()
+            
         }
         .padding()
     }
@@ -23,4 +27,36 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct MapView: UIViewRepresentable {
+  typealias UIViewType = MKMapView
+  
+    func makeCoordinator() -> MapViewCoordinator {
+      return MapViewCoordinator()
+    }
+    
+  func makeUIView(context: Context) -> MKMapView {
+    let mapView = MKMapView()
+      mapView.delegate = context.coordinator
+      let region = MKCoordinateRegion(
+      center: CLLocationCoordinate2D(latitude: 52.23185930, longitude: 21.00677160),
+      span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+    mapView.setRegion(region, animated: true)
+
+    return mapView
+  }
+
+  func updateUIView(_ uiView: MKMapView, context: Context) {}
+    
+    class MapViewCoordinator: NSObject, MKMapViewDelegate {
+      func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+        let renderer = MKPolylineRenderer(overlay: overlay)
+        renderer.strokeColor = .systemBlue
+        renderer.lineWidth = 5
+        return renderer
+      }
+    }
+    
 }
