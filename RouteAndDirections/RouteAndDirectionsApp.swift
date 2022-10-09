f9e199ae50f55234a4623c9733838f1553715ad4
Jacek Kosiński <kashiash@gmail.com>
2022-10-09 16:08:35 +0200

    Initial Commit

---
+++ RouteAndDirections/RouteAndDirectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouteAndDirectionsApp.swift
+//  RouteAndDirections
+//
+//  Created by Jacek Kosinski U on 09/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct RouteAndDirectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
